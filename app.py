b929b94c6025ed53dee0f0bc88bfbde9fb872160
paraboxic <105340879+paraboxic@users.noreply.github.com>
2022-07-25 00:05:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+#div
+
 import streamlit as st
 
-st.header("Week 8 Assignment- adwait  bapat- 21f2000301")
+st.title('This app divides the two numbers')
+a= st.number_input('Number A')
 
-number1 = st.number_input('Insert first number',value=0) 
 
-number2 = st.number_input('Insert second number',value=0)
-
-if number1 is not None and number2 is not None:
-    st.write("Sum of the numbers is: {}".format((number1+number2)))
+b =st.number_input('Number B')
+if b == 0:
+  a1=st.write(f'A/B = Undefined')
+else:
+  a1=st.write(f'A/B = {a/b}')
+print(a1)
